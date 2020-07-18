334c8a6db49b7b53553b293bff099230af7b6a99
peterjuma <peterjuma@github.com>
2020-07-18 13:40:44 +0300

    Update createmultifiles.sh

--- createmultifiles.sh
+++ createmultifiles.sh
@@ -4,8 +4,7 @@ stub="File000"
 count=${1:-$default}
 
 for (( i=1; i<$count+1; i++))
-do 
+do
   filename=$stub$i".txt"
-  echo $filename
-  echo $(date +%F_%H-%M-%S) This is my message of the day  > $filename
+  [ -f $filename ] && echo "$filename exists, moving on" || { echo $(date +%F_%H-%M-%S) This is my message of the day  > $filename; echo "$filename created"; }
 done
