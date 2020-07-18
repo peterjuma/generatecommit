3b8336d3de67cb736e753900ed61b24c8e578e6e
peterjuma <peterjuma@github.com>
2020-07-18 18:14:40 +0300

    Update bigassfile.sh

--- bigassfile.sh
+++ bigassfile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rand=$(awk 'BEGIN{srand();print int(rand()*(63000-2000))+2000 }')
 text="Cras varius. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Fusce pharetra convallis urna."
-yes $text | head -n 100000 > somebigassfile.txt
+yes $text | head -n 100000 > bigassfile${rand}.txt
