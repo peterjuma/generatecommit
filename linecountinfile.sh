4c1f46b6aad8b94dae6f524c9b606672c88cc2c1
peterjuma <peterjuma@github.com>
2020-07-19 14:01:19 +0300

    Update linecountinfile.sh

--- linecountinfile.sh
+++ linecountinfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 default=10
 count=${1:-$default}
-filename=${2:-"file-with-$count-lines.rb"}
+filename=${2:-"file-with-$count-lines.txt"}
 for (( i=1; i<$count+1; i++)) do 
 	echo -e "#Vivamus in erat ut urna cursus vestibulum. Morbi ac felis. Phasellus ullamcorper ipsum rutrum nunc. In auctor lobortis lacus."  >> $filename
 done
