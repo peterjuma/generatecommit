9df33ce170ee8c59775448c9a6e941cf52d6c4fa
peterjuma <peterjuma@github.com>
2020-07-19 14:47:42 +0300

    Update commits.sh

--- commits.sh
+++ commits.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 default=1
+stub=${2:-"File000*"}
 count=${1:-$default}
+
 for (( i=1; i<$count+1; i++))
 do
-	sh ./editfiles.sh File000*
+	sh ./editfiles.sh $stub
 	COMMIT=$(python3 commitmsg.py)
 	DATE=$(date +%d/%m/%Y@%T)
 	COMMITMSG=$(echo $COMMIT | cut -d"|" -f1)
 	COMMITMSGURL=$(echo $COMMIT | cut -d"|" -f2)
-	git add File000*
+	git add $stub
 	git commit -a -m "${COMMITMSG}"
 done
