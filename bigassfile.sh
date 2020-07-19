4bdde11af67c828d7b115c0516b47c3712375ba4
peterjuma <peterjuma@github.com>
2020-07-19 14:23:03 +0300

    Update bigassfile.sh

--- bigassfile.sh
+++ bigassfile.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+default=1048576
+stub=${2:-"BigAssFile"}
+count=${1:-$default}
+
 rand=$(awk 'BEGIN{srand();print int(rand()*(63000-2000))+2000 }')
 
 text="Phasellus a est. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna. Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus. Praesent turpis. Quisque id mi.\n
@@ -10,4 +15,4 @@ Phasellus consectetuer vestibulum elit.\n
 
 Etiam feugiat lorem non metus.\n"
 
-yes $text | head -n 1100000 > bigassfile${rand}.txt
+yes $text | head -n $count > $stub${rand}.txt
