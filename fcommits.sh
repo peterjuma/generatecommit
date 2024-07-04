ec60cad962865df1f42be6a3d25cd487911623a8
Peter Juma <30427827+peterjuma@users.noreply.github.com>
2024-07-04 11:47:24 +0300

    Done, to whoever merges this, good luck.

--- fcommits.sh
+++ fcommits.sh
@@ -2,28 +2,21 @@
 
 # default=1
 # count=${1:-1}
-COUNT=${1:-1}
-
-sh commits.sh 1
+# COUNT=${1:-1}
 
 FILE=$(lipsum -n 1 -w 1 | awk '{print tolower($0) ext}' | awk '{gsub(/\.|\ /,"")}1')
 FILENAME=${2:-"${FILE}.md"}
 
-if [ $# -eq 0 ]
-  then
-    COMMITMSG=$(python3 commitmsg.py)
-    git add .
-    git commit --amend -m "${COMMITMSG}"
-    git push --force
-    exit 1
-fi
 
-for (( i=1; i<=$COUNT; i++ ))
-do
-    sh ./editfiles.sh $FILENAME
-    COMMITMSG=$(python3 commitmsg.py)
-    git add $FILENAME
-    git commit --amend -m "${COMMITMSG}"
-done
+sh ./editfiles.sh $FILENAME
+COMMITMSG=$(python3 commitmsg.py)
+#COMMITMSG=$(curl -s http://whatthecommit.com/ | grep -A2 '<div id="content">' | awk -F: 'NR==2 {sub(/<p>/,"");print $1}')
+git add $FILENAME
+git commit -a -m "${COMMITMSG}"
+git push
 
+
+COMMITMSG=$(python3 commitmsg.py)
+git commit --amend -m "${COMMITMSG}"
 git push --force
+
